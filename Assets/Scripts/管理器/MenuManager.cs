684e90aa0174e4f2fff555aee9d14929a0439971
jokicc123 <a0903010911@gmail.com>
2026-05-18 23:17:47 +0800

    更新
    設定音效至遊戲場景

--- Assets/Scripts/管理器/MenuManager.cs
+++ Assets/Scripts/管理器/MenuManager.cs
@@ -7,67 +7,37 @@ namespace CHANG
     ///主選單管理器
     /// </summary>
     public class MenuManager : MonoBehaviour
-
     {
-        private Button btnContinue, btnNewGame, btnOption, btnCredit, btnQuit, btnBackOption, btnBackCredit;
-
-        private CanvasGroup groupMain, groupOption, groupCredit;
+        private Button btnContinue, btnNewGame,  btnCredit, btnQuit,btnBackCredit;
+        private CanvasGroup groupMain, groupCredit;
         private Slider sliderMaster, sliderBgm, sliderSfx;
-
         private void Awake()
         {
             #region  尋找介面物件
             btnNewGame = GameObject.Find("按鈕_開始遊戲").GetComponent<Button>();
-            btnOption = GameObject.Find("按鈕_設定").GetComponent<Button>();
             btnCredit = GameObject.Find("按鈕_製作團隊").GetComponent<Button>();
             btnQuit = GameObject.Find("按鈕_退出遊戲").GetComponent<Button>();
-            btnBackOption = GameObject.Find("按鈕_設定_返回").GetComponent<Button>();
             btnBackCredit = GameObject.Find("按鈕_製作團隊_返回").GetComponent<Button>();
-            groupOption = GameObject.Find("群組_設定").GetComponent<CanvasGroup>();
             groupCredit = GameObject.Find("群組_製作團隊").GetComponent<CanvasGroup>();
             sliderMaster = GameObject.Find("滑桿_主音量").GetComponent<Slider>();
             sliderBgm = GameObject.Find("滑桿_音樂").GetComponent<Slider>();
             sliderSfx = GameObject.Find("滑桿_音效").GetComponent<Slider>();
-
-
             #endregion
-            btnOption.onClick.AddListener(() =>
-            {
-                StartCoroutine(FadeSystem.Fade(groupOption));
-
-            });
+       
             btnCredit.onClick.AddListener(() =>
             {
                 StartCoroutine(FadeSystem.Fade(groupCredit));
-
             });
-            btnBackOption.onClick.AddListener(() =>
-            {
-                StartCoroutine(FadeSystem.Fade(groupOption, false));
-
-            });
+           
             btnBackCredit.onClick.AddListener(() =>
             {
                 StartCoroutine(FadeSystem.Fade(groupCredit, false));
-
             });
             btnNewGame.onClick.AddListener(NewGame);
             btnQuit.onClick.AddListener(Quit);
-
-            sliderMaster.onValueChanged.AddListener(x => SoundManager.Instance.UpdateMasterVolume(x));
-            sliderBgm.onValueChanged.AddListener(x => SoundManager.Instance.UpdateBGMVolume(x));
-            sliderSfx.onValueChanged.AddListener(x => SoundManager.Instance.UpdateSFXVolume(x));
-
-            sliderMaster.value = SoundManager.Instance.VolumeMaster;
-            sliderBgm.value = SoundManager.Instance.VolumeBGM;
-            sliderSfx.value = SoundManager.Instance.VolumeSFX;
-
-
-
         }
         private void Continue()
         {
-
         }
         private void NewGame()
         {
---
+++ Assets/Scripts/管理器/SettingsPanel.cs
@@ -0,0 +1,40 @@
+﻿using CHANG;
+using UnityEngine;
+using UnityEngine.UI;
+public class SettingsPanel: MonoBehaviour
+{
+    private Button btn, btnSettings, btnBack;
+    private CanvasGroup groupOption;
+    private Slider sliderMaster, sliderBgm, sliderSfx;
+
+    private void Awake()
+    {
+        btnSettings = GameObject.Find("按鈕_設定").GetComponent<Button>();
+        btnBack = GameObject.Find("按鈕_設定_返回").GetComponent<Button>();
+        groupOption = GameObject.Find("群組_設定").GetComponent<CanvasGroup>();
+        sliderMaster = GameObject.Find("滑桿_主音量").GetComponent<Slider>();
+        sliderBgm = GameObject.Find("滑桿_音樂").GetComponent<Slider>();
+        sliderSfx = GameObject.Find("滑桿_音效").GetComponent<Slider>();
+
+        btnSettings.onClick.AddListener(() =>
+        {
+            StartCoroutine(FadeSystem.Fade(groupOption));
+        });
+        btnBack.onClick.AddListener(() =>
+        {
+            StartCoroutine(FadeSystem.Fade(groupOption, false));
+        });
+        sliderMaster.onValueChanged.AddListener(x => SoundManager.Instance.UpdateMasterVolume(x));
+        sliderBgm.onValueChanged.AddListener(x => SoundManager.Instance.UpdateBGMVolume(x));
+        sliderSfx.onValueChanged.AddListener(x => SoundManager.Instance.UpdateSFXVolume(x));
+
+       
+    }
+    private void Start()
+    {
+        sliderMaster.value = SoundManager.Instance.VolumeMaster;
+        sliderBgm.value = SoundManager.Instance.VolumeBGM;
+        sliderSfx.value = SoundManager.Instance.VolumeSFX;
+    }
+   
+}
